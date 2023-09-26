618214b0098e0bc3bc7dd39aed92efcc3fa0040c
azzurik <val.louvet@gmail.com>
2023-09-26 13:56:09 +0200

    fix ecs role

--- ecs.tf
+++ ecs.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role" "ecs_execution_role" {
       },
     ],
   })
-  managed_policy_arns = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
 }
 
 resource "aws_ecs_service" "service" {
